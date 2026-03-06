166ab8286e59fdde29b3ea15c21d616c597755a0
Prathmesh <uttekarprathmesh30@gmail.com>
2026-03-06 07:20:11 +0530

    change

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "students_db");
+$conn = mysqli_connect("localhost", "root", "", "student_db");
 
 if (!$conn) {
     die("Connection failed");
